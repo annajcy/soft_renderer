55b3540f3e14cb996f17db48a9d4a92240637ed2
jcy <2821872892@qq.com>
2024-09-14 00:27:13 +0800

    removed test cmakelists

---
+++ test/rasterization/rasterize_line_bresenham_colored_test.cpp
@@ -0,0 +1,38 @@
+#include "application.h"
+#include "gpu.h"
+#include "raster.h"
+#include "maths.h"
+#include "base.h"
+#include "color.h"
+
+std::string app_id = "soft_renderer";
+int height = 400;
+int width = 800;
+int cnt = 0;
+
+int main()
+{
+    app->init(width, height, app_id);
+	gpu->init(app->canvas);
+
+	std::vector<math::Pixel_color> line;
+	Raster::rasterize_line_bresenham_colored(line, {{100, 100}, Color(255, 0, 0, 255)}, {{300, 200}, Color(0, 255, 0, 255)});
+
+    while (app->active) {
+
+	    gpu->clear();
+
+	    for (auto &[p, color] : line) {
+		    gpu->set_pixel(p.x(), p.y(), color);
+		    std::cout << p.x() << ' ' << p.y() << color << std::endl;
+	    }
+
+        app->update();
+        auto message = app->get_message();
+        app->handle_message(message);
+    }
+
+	app->exit();
+
+    return 0;
+}
