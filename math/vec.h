d0bb4df4c7eb3b4e5aec35c6ed9d27ca46369e5c
jcy <2821872892@qq.com>
2024-09-16 00:07:54 +0800

    modify vec.h

--- math/vec.h
+++ math/vec.h
@@ -10,6 +10,15 @@ namespace math {
 		std::unique_ptr<T[]> data;
 
 	public:
+
+		static Vec<T, S>zeros() {
+			return Vec<T, S>(0);
+		}
+
+		static Vec<T, S>ones() {
+			return Vec<T, S>(1);
+		}
+
 		Vec<T, S>() : data(std::make_unique<T[]>(S)) { }
 
 		explicit Vec<T, S>(int value) : data(std::make_unique<T[]>(S)) {
@@ -39,6 +48,38 @@ namespace math {
 			}
 		}
 
+		Vec<T, S>& operator=(const Vec<T, S>& rhs) {
+			if (this == &rhs) return *this;
+			for (int i = 0; i < S; i ++)
+				this->data[i] = rhs[i];
+			return *this;
+		}
+
+		template<typename Q, int N>
+		Vec<T, S>& operator=(const Vec<Q, N>& rhs) {
+			for (int i = 0; i < std::min(N, S); i ++)
+				this->data[i] = rhs[i];
+			return *this;
+		}
+
+		Vec<T, S>& operator=(Vec<T, S>&& rhs) noexcept {
+			if (this == &rhs) return *this;
+			this->data = std::move(rhs.data);
+			return *this;
+		}
+
+		T& x() requires greater_than<int, S, 1> { return data[0]; }
+		T& y() requires greater_than<int, S, 2> { return data[1]; }
+		T& z() requires greater_than<int, S, 3> { return data[2]; }
+		T& w() requires greater_than<int, S, 4> { return data[3]; }
+
+		T x() const requires greater_than<int, S, 1> { return data[0]; }
+		T y() const requires greater_than<int, S, 2> { return data[1]; }
+		T z() const requires greater_than<int, S, 3> { return data[2]; }
+		T w() const requires greater_than<int, S, 4> { return data[3]; }
+
+		int dims() const { return S; }
+
 		T operator[](int index) const {
 			if (index < 0 || index >= S) throw std::out_of_range("Index out of range");
 			return data[index];
@@ -59,40 +100,6 @@ namespace math {
 			for (int i = 0; i < S; i ++) 
 				if (!equal(this->data[i], vec[i])) return false;
 			return true;
-		}
-
-		T& x() requires greater_than<int, S, 1> { return data[0]; }
-		T& y() requires greater_than<int, S, 2> { return data[1]; }
-		T& z() requires greater_than<int, S, 3> { return data[2]; }
-		T& w() requires greater_than<int, S, 4> { return data[3]; }
-
-		T x() const requires greater_than<int, S, 1> { return data[0]; }
-		T y() const requires greater_than<int, S, 2> { return data[1]; }
-		T z() const requires greater_than<int, S, 3> { return data[2]; }
-		T w() const requires greater_than<int, S, 4> { return data[3]; }
-
-		int dims() const {
-			return S;
-		}
-
-		Vec<T, S>& operator=(const Vec<T, S>& rhs) {
-			if (this == &rhs) return *this;
-			for (int i = 0; i < S; i ++)
-				this->data[i] = rhs[i];
-			return *this;
-		}
-
-		template<typename Q, int N>
-		Vec<T, S>& operator=(const Vec<Q, N>& rhs) {
-			for (int i = 0; i < std::min(N, S); i ++)
-				this->data[i] = rhs[i];
-			return *this;
-		}
-
-		Vec<T, S>& operator=(Vec<T, S>&& rhs) noexcept {
-			if (this == &rhs) return *this;
-			this->data = std::move(rhs.data);
-			return *this;
 		}
 
 		Vec<T, S> operator-() const {
@@ -126,6 +133,14 @@ namespace math {
 			return result;
 		}
 
+		template<typename Q>
+		Vec<T, S> operator*(const Vec<Q, S>& rhs) const {
+			Vec<T, S> result;
+			for (int i = 0; i < S; i ++)
+				result[i] = this->data[i] * rhs[i];
+			return result;
+		}
+
 		template<typename N>
 		Vec<T, S> operator/(const N& rhs) const {
 			if (!rhs) throw std::runtime_error("Divided by zero");
@@ -156,6 +171,13 @@ namespace math {
 			return *this;
 		}
 
+		template<typename Q>
+		Vec<T, S> operator*(const Vec<Q, S>& rhs) {
+			for (int i = 0; i < S; i ++)
+				this->data[i] *= rhs[i];
+			return *this;
+		}
+
 		template<class N>
 		Vec<T, S>& operator/=(const N& rhs) {
 			if (!rhs) throw std::runtime_error("Divided by zero");
@@ -169,6 +191,15 @@ namespace math {
 			T result = 0;
 			for (int i = 0; i < S; i ++)
 				result += this->data[i] * v[i];
+			return result;
+		}
+
+		Vec<T, S> inv() const {
+			Vec<T, S> result;
+			for (int i = 0; i < S; i ++) {
+				if (!this->data[i]) throw std::runtime_error("Divided by zero");
+				result[i] = 1.0 / this->data[i];
+			}
 			return result;
 		}
 
@@ -204,24 +235,6 @@ namespace math {
 		}
 
 		template<typename N>
-		friend Vec<T, S> inv(const Vec<N, S>& vec) {
-			Vec<T, S> result;
-			for (int i = 0; i < S; i ++) {
-				if (!vec[i]) throw std::runtime_error("Divided by zero");
-				result[i] = 1.0 / vec[i];
-			}
-			return result;
-		}
-
-		template<typename Q>
-		friend Vec<T, S> operator*(const Vec<T, S>& lhs, const Vec<Q, S>& rhs) {
-			Vec<T, S> result;
-			for (int i = 0; i < S; i ++)
-				result[i] = lhs[i] * rhs[i];
-			return result;
-		}
-
-		template<typename N>
 		friend Vec<T, S> operator*(const N& lhs,const Vec<T, S>& rhs) {
 			Vec<T, S> result;
 			for (int i = 0; i < S; i ++)
@@ -229,7 +242,6 @@ namespace math {
 			return result;
 		}
 
-		//C++ 20
 		template<typename Q, int N>
 		friend Vec<Q, N> cast(const Vec<T, S>& vec) {
 			Vec<Q, N> result;
