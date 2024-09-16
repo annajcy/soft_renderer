f3b37e26b919971d582d77a39f7b57b019fb03af
jcy <2821872892@qq.com>
2024-09-16 11:40:58 +0800

    change unique_ptr to std::array in vec.h

--- math/vec.h
+++ math/vec.h
@@ -1,70 +1,66 @@
 #pragma once
 
 #include "base.h"
+#include <array>
 
 namespace math {
-	
+
 	template<arithmetic T, int S>
 	class Vec {
 	private:
-		std::unique_ptr<T[]> data;
+		std::array<T, S> data;
 
 	public:
 
-		static Vec<T, S>zeros() {
+		static Vec<T, S> zeros() {
 			return Vec<T, S>(0);
 		}
 
-		static Vec<T, S>ones() {
+		static Vec<T, S> ones() {
 			return Vec<T, S>(1);
 		}
 
-		Vec<T, S>() : data(std::make_unique<T[]>(S)) { }
+		Vec<T, S>() : data{} { }
 
-		explicit Vec<T, S>(int value) : data(std::make_unique<T[]>(S)) {
-			for (int i = 0; i < S; i ++)
-				data[i] = value;
+		explicit Vec<T, S>(int value) {
+			data.fill(value);
 		}
 
-		Vec<T, S>(const Vec<T, S>& vec) : data(std::make_unique<T[]>(S)) {
-			for (int i = 0; i < S; i ++)
-				data[i] = vec[i];
-		}
+		Vec<T, S>(const Vec<T, S>& vec) : data(vec.data) { }
 
 		template<typename Q, int N>
-		explicit Vec<T, S>(const Vec<Q, N>& vec) : data(std::make_unique<T[]>(S)) {
-			for (int i = 0; i < std::min(N, S); i ++)
+		explicit Vec<T, S>(const Vec<Q, N>& vec) {
+			for (int i = 0; i < std::min(N, S); i++)
 				data[i] = vec[i];
 		}
 
 		Vec<T, S>(Vec<T, S>&& vec) noexcept : data(std::move(vec.data)) { }
 
 		template<typename N>
-		Vec<T, S>(const std::initializer_list<N>& list) : data(std::make_unique<T[]>(S)) {
+		Vec<T, S>(const std::initializer_list<N>& list) {
 			int cnt = 0;
 			for (auto i : list) {
-				data[cnt ++] = i;
+				data[cnt++] = i;
 				if (cnt == S) return;
 			}
 		}
 
 		Vec<T, S>& operator=(const Vec<T, S>& rhs) {
 			if (this == &rhs) return *this;
-			for (int i = 0; i < S; i ++)
-				this->data[i] = rhs[i];
+			data = rhs.data;
 			return *this;
 		}
 
 		template<typename Q, int N>
 		Vec<T, S>& operator=(const Vec<Q, N>& rhs) {
-			for (int i = 0; i < std::min(N, S); i ++)
-				this->data[i] = rhs[i];
+			for (int i = 0; i < std::min(N, S); i++)
+				data[i] = rhs[i];
 			return *this;
 		}
 
 		Vec<T, S>& operator=(Vec<T, S>&& rhs) noexcept {
 			if (this == &rhs) return *this;
-			this->data = std::move(rhs.data);
+			data = std::move(rhs.data);
 			return *this;
 		}
 
@@ -91,53 +87,51 @@ namespace math {
 		}
 
 		bool operator==(const Vec<T, S>& vec) const {
-			for (int i = 0; i < S; i ++) 
-				if (this->data[i] != vec[i]) return false;
-			return true;
+			return data == vec.data;
 		}
 
 		bool operator==(const Vec<T, S>& vec) const requires floating_point<T> {
-			for (int i = 0; i < S; i ++) 
+			for (int i = 0; i < S; i++)
 				if (!equal(this->data[i], vec[i])) return false;
 			return true;
 		}
 
 		Vec<T, S> operator-() const {
 			Vec<T, S> result;
-			for (int i = 0; i < S; i ++)
-				result[i] = -this->data[i];
+			for (int i = 0; i < S; i++)
+				result[i] = -data[i];
 			return result;
 		}
 
 		template<typename N>
 		Vec<T, S> operator+(const Vec<N, S>& rhs) const {
 			Vec<T, S> result;
-			for (int i = 0; i < S; i ++)
-				result[i] = this->data[i] + rhs[i];
+			for (int i = 0; i < S; i++)
+				result[i] = data[i] + rhs[i];
 			return result;
 		}
 
 		template<typename N>
 		Vec<T, S> operator-(const Vec<N, S>& rhs) const {
 			Vec<T, S> result;
-			for (int i = 0; i < S; i ++)
-				result[i] = this->data[i] - rhs[i];
+			for (int i = 0; i < S; i++)
+				result[i] = data[i] - rhs[i];
 			return result;
 		}
 
 		template<typename N>
 		Vec<T, S> operator*(const N& scalar) const {
 			Vec<T, S> result;
-			for (int i = 0; i < S; i ++)
-				result[i] = this->data[i] * scalar;
+			for (int i = 0; i < S; i++)
+				result[i] = data[i] * scalar;
 			return result;
 		}
 
 		template<typename Q>
 		Vec<T, S> operator*(const Vec<Q, S>& rhs) const {
 			Vec<T, S> result;
-			for (int i = 0; i < S; i ++)
-				result[i] = this->data[i] * rhs[i];
+			for (int i = 0; i < S; i++)
+				result[i] = data[i] * rhs[i];
 			return result;
 		}
 
@@ -145,99 +139,82 @@ namespace math {
 		Vec<T, S> operator/(const N& rhs) const {
 			if (!rhs) throw std::runtime_error("Divided by zero");
 			Vec<T, S> result;
-			for (int i = 0; i < S; i ++)
-				result[i] = this->data[i] * 1.0 / rhs;
+			for (int i = 0; i < S; i++)
+				result[i] = data[i] / rhs;
 			return result;
 		}
 
 		template<typename N>
 		Vec<T, S>& operator+=(const Vec<N, S>& rhs) {
-			for (int i = 0; i < S; i ++)
-				this->data[i] += rhs;
+			for (int i = 0; i < S; i++)
+				data[i] += rhs[i];
 			return *this;
 		}
 
 		template<typename N>
 		Vec<T, S>& operator-=(const Vec<N, S>& rhs) {
-			for (int i = 0; i < S; i ++)
-				this->data[i] -= rhs;
+			for (int i = 0; i < S; i++)
+				data[i] -= rhs[i];
 			return *this;
 		}
 
 		template<typename N>
 		Vec<T, S>& operator*=(const N& rhs) {
-			for (int i = 0; i < S; i ++)
-				this->data[i] *= rhs;
-			return *this;
-		}
-
-		template<typename Q>
-		Vec<T, S> operator*(const Vec<Q, S>& rhs) {
-			for (int i = 0; i < S; i ++)
-				this->data[i] *= rhs[i];
+			for (int i = 0; i < S; i++)
+				data[i] *= rhs;
 			return *this;
 		}
 
 		template<class N>
 		Vec<T, S>& operator/=(const N& rhs) {
 			if (!rhs) throw std::runtime_error("Divided by zero");
-			for (int i = 0; i < S; i ++) {
-				this->data[i] *= 1.0 / rhs;
-			}
+			for (int i = 0; i < S; i++)
+				data[i] /= rhs;
 			return *this;
 		}
 
 		T dot(const Vec<T, S>& v) const {
 			T result = 0;
-			for (int i = 0; i < S; i ++)
-				result += this->data[i] * v[i];
-			return result;
-		}
-
-		Vec<T, S> inv() const {
-			Vec<T, S> result;
-			for (int i = 0; i < S; i ++) {
-				if (!this->data[i]) throw std::runtime_error("Divided by zero");
-				result[i] = 1.0 / this->data[i];
-			}
+			for (int i = 0; i < S; i++)
+				result += data[i] * v[i];
 			return result;
 		}
 
 		decimal norm() const {
 			decimal result = 0;
-			for (int i = 0; i < S; i ++) 
-				result += this->data[i] * this->data[i];
+			for (int i = 0; i < S; i++)
+				result += data[i] * data[i];
 			return std::sqrt(result);
 		}
 
 		Vec<decimal, S> normalize() const {
 			decimal norm = this->norm();
 			Vec<decimal, S> result;
-			for (int i = 0; i < S; i ++) 
-				result[i] = this->data[i] / norm;
+			for (int i = 0; i < S; i++)
+				result[i] = data[i] / norm;
 			return result;
 		}
 
 		Vec<decimal, S> project_to(const Vec<T, S>& vec) const {
 			Vec<decimal, S> result;
 			auto vec_norm = vec.norm();
-			decimal factor = this->dot(vec) / vec_norm / vec_norm;
-			for (int i = 0; i < S; i ++) 
+			decimal factor = this->dot(vec) / (vec_norm * vec_norm);
+			for (int i = 0; i < S; i++)
 				result[i] = vec[i] * factor;
 			return result;
 		}
 
 		friend std::ostream& operator<<(std::ostream& os, const Vec<T, S>& vec) {
-			for (int i = 0; i < S; i ++)
-				std::cout << vec.data[i] << ' ';
-			std::cout << std::endl;
+			for (int i = 0; i < S; i++)
+				os << vec.data[i] << ' ';
+			os << std::endl;
 			return os;
 		}
 
 		template<typename N>
-		friend Vec<T, S> operator*(const N& lhs,const Vec<T, S>& rhs) {
+		friend Vec<T, S> operator*(const N& lhs, const Vec<T, S>& rhs) {
 			Vec<T, S> result;
-			for (int i = 0; i < S; i ++)
+			for (int i = 0; i < S; i++)
 				result[i] = lhs * rhs[i];
 			return result;
 		}
@@ -245,7 +222,7 @@ namespace math {
 		template<typename Q, int N>
 		friend Vec<Q, N> cast(const Vec<T, S>& vec) {
 			Vec<Q, N> result;
-			for (int i = 0; i < std::min(N, S); i ++)
+			for (int i = 0; i < std::min(N, S); i++)
 				result[i] = vec[i];
 			return result;
 		}
@@ -253,7 +230,7 @@ namespace math {
 		template<typename Q>
 		friend Vec<Q, S> cast_type(const Vec<T, S>& vec) {
 			Vec<Q, S> result;
-			for (int i = 0; i < S; i ++)
+			for (int i = 0; i < S; i++)
 				result[i] = vec[i];
 			return result;
 		}
@@ -261,18 +238,17 @@ namespace math {
 		template<int N>
 		friend Vec<T, N> cast_dims(const Vec<T, S>& vec) {
 			Vec<T, N> result;
-			for (int i = 0; i < std::min(N, S); i ++)
+			for (int i = 0; i < std::min(N, S); i++)
 				result[i] = vec[i];
 			return result;
 		}
-
 	};
-	
+
 	using Vec2f = Vec<decimal, 2>;
 	using Vec2i = Vec<int, 2>;
 	using Vec3f = Vec<decimal, 3>;
 	using Vec3i = Vec<int, 3>;
 	using Vec4f = Vec<decimal, 4>;
-	using Vec4i = Vec<int, 4>;	
+	using Vec4i = Vec<int, 4>;
 
 }
